6caf221e46013337e6a607bb09e381a4029d282a
konstantin0s <constantintofan85@gmail.com>
2019-07-25 11:01:51 +0200

    Refactor functions

--- javascript/main.js
+++ javascript/main.js
@@ -1,107 +1,124 @@
-// var chronometer = new Chronometer();
-var btnLeft = document.getElementById('btnLeft');
-var btnRight = document.getElementById('btnRight');
-var minDec = document.getElementById('minDec');
-var minUni = document.getElementById('minUni');
-var secDec = document.getElementById('secDec');
-var secUni = document.getElementById('secUni');
-var milDec = document.getElementById('milDec');
-var milUni = document.getElementById('milUni');
-var addSplits = document.getElementById('splits');
-var recordBtn = document.getElementById('btn split');
-var listItem = document.getElementsByClassName('list-item');
+(function () {
+
+  // let chronometer = new Chronometer();
+  let btnLeft = document.getElementById('btnLeft');
+  let btnRight = document.getElementById('btnRight');
+  let minDec = document.getElementById('minDec');
+  let minUni = document.getElementById('minUni');
+  let secDec = document.getElementById('secDec');
+  let secUni = document.getElementById('secUni');
+  let milDec = document.getElementById('milDec');
+  let milUni = document.getElementById('milUni');
+  let addSplits = document.getElementById('splits');
+  let recordBtn = document.getElementById('btn split');
+  let listItem = document.getElementsByClassName('list-item');
 
 
-var currentTime = 0; //takes split time..
-var intervalId = 0;  //0stop; 1 running.
-var seconds = 0;
-var minutes = 0;
-var miliseconds = 0;
-var intervalRef = null;
+  let currentTime = 0; //takes split time..
+  let intervalId = 0;  //0stop; 1 running.
+  let seconds = 0;
+  let minutes = 0;
+  let miliseconds = 0;
+  let intervalRef = null;
 
-//add 00 on the html before starts counting.
-secDec.innerHTML = "0" + seconds;
-minDec.innerHTML = "0" + minutes;
-milDec.innerHTML = miliseconds;
-
-
-function twoDigits(number) {
-  if (number < 10) {
-    return "0" + number;
+  //add 00 on the html before starts counting.
+  function startWithZero() {
+    secDec.innerHTML = "0" + seconds;
+    minDec.innerHTML = "0" + minutes;
+    milDec.innerHTML = miliseconds;
   }
-  return number;
-}
 
-btnLeft.addEventListener("click", function (event) {
-  if (intervalId == 0) { //start if pause
-    intervalRef = setInterval(function () {
-      intervalId = 1;
-      miliseconds++;
-      milDec.innerHTML = twoDigits(miliseconds);
-      seconds++;
-      secDec.innerHTML = twoDigits(seconds);
-      btnLeft.innerHTML = "STOP";
-      btnLeft.setAttribute("class", "btn stop");
-      btnRight.setAttribute("class", "btn split");
-      btnRight.innerHTML = "SPLIT";
 
-      if (miliseconds === 60) {
-        miliseconds = 0;
-        milDec.innerHTML = twoDigits(miliseconds)
-      }
+  function twoDigits(number) {
+    if (number < 10) {
+      return "0" + number;
+    }
+    return number;
+  }
 
-      if (seconds === 60) {
-        seconds = 0;
-        minutes++;
-        minDec.innerText = twoDigits(minutes);
-        secDec.innerText = "0" + seconds;
-        if (minDec === 60) {
-          minutes = 0;
-        }
-      }
-    }, 100);
-  } else { //pause if started
-    clearInterval(intervalRef);  //pause chronometer
+  startWithZero();
+  btnLeft.addEventListener("click", startButton);
+  btnRight.addEventListener("click", doTheSplit);
+
+  function textStartBtn() {
+    btnLeft.innerHTML = "STOP";
+    btnLeft.setAttribute("class", "btn stop");
+    btnRight.setAttribute("class", "btn split");
+    btnRight.innerHTML = "SPLIT";
+  }
+
+  function createResetBtn() {
     btnLeft.innerHTML = "START";
     btnRight.innerHTML = "RESET";
     btnLeft.setAttribute("class", "btn start");
     btnRight.setAttribute("class", "btn reset");
     intervalId = 0; // continue chronometer from it's pause point.
   }
-});
 
-btnRight.addEventListener("click", function () {
-  btnRight.style.backgroundColor = "grey";
-  var li = document.createElement("li");
-  li.setAttribute("class", "list-item");
-  li.innerHTML = twoDigits(minutes) + ":" + twoDigits(seconds) + ":" + twoDigits(miliseconds);
-  addSplits.appendChild(li);
-
-  var testClass = document.getElementById('btnRight');
-  console.log(testClass);
-  if (testClass.classList.contains("reset")) {
-    if (!testClass.classList.contains("split")) {
-      reset();
+  function startButton() {
+    if (intervalId == 0) { //start if pause
+      intervalRef = setInterval(function () {
+        intervalId = 1;
+        miliseconds++;
+        milDec.innerHTML = twoDigits(miliseconds);
+        if (miliseconds === 60) {
+          miliseconds = 0;
+          milDec.innerHTML = twoDigits(miliseconds)
+        }
+        seconds++;
+        secDec.innerHTML = twoDigits(seconds);
+        if (seconds === 60) {
+          seconds = 0;
+          minutes++;
+          minDec.innerText = twoDigits(minutes);
+          secDec.innerText = "0" + seconds;
+          if (minDec === 60) {
+            minutes = 0;
+          }
+        }
+        textStartBtn();
+      }, 100);
+    } else { //pause if started
+      clearInterval(intervalRef);  //pause chronometer
+      createResetBtn();
     }
   }
-});
 
-function reset() {
-  clearInterval(intervalRef);
-  intervalId = 0;
-  milisecods = 0;
-  seconds = 0;
-  minutes = 0;
-  btnLeft.innerHTML = "START";
 
-  // sets values of them to 00 after reset
-  minDec.innerHTML = "0" + minutes;
-  secDec.innerHTML = "0" + seconds;
-  milDec.innerHTML = milisecods;
-  listItem.innerHTML = 0;
-  timedRefresh(2000);
-}
+  function doTheSplit() {
+    btnRight.style.backgroundColor = "grey";
+    let li = document.createElement("li");
+    li.setAttribute("class", "list-item");
+    li.innerHTML = twoDigits(minutes) + ":" + twoDigits(seconds) + ":" + twoDigits(miliseconds);
+    addSplits.appendChild(li);
+    changeClassDoTheSplit();
+  }
 
-function timedRefresh(timeoutPeriod) {
-  setTimeout("location.reload(true);", timeoutPeriod);
-}
+  function changeClassDoTheSplit() {
+    let testClass = document.getElementById('btnRight');
+    if (testClass.classList.contains("reset")) {
+      if (!testClass.classList.contains("split")) {
+        reset();
+      }
+    }
+  }
+
+  function reset() {
+    clearInterval(intervalRef);
+    intervalId = 0;
+    milisecods = 0;
+    seconds = 0;
+    minutes = 0;
+    btnLeft.innerHTML = "START";
+
+    // sets values of them to 00 after reset
+    startWithZero();
+    listItem.innerHTML = 0;
+    timedRefresh(2000);
+  }
+
+  function timedRefresh(timeoutPeriod) {
+    setTimeout("location.reload(true);", timeoutPeriod);
+  }
+
+}());
